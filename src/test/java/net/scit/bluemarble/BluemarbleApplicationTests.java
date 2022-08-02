091d8825b0ecf8f0dbe970189cf58b2bcb0bcfa1
jinw0909 <jinw0909@gmail.com>
2022-08-02 20:11:14 +0900

    bluemarble spring application

---
+++ src/test/java/net/scit/bluemarble/BluemarbleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.scit.bluemarble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluemarbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
