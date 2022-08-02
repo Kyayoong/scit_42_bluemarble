d33254ec6b08de318cb9142f20acac0860aece78
SoungJun Cho <gksrnr0529@gmail.com>
2022-08-02 16:38:59 +0900

    spring files

---
+++ bluemarble/src/test/java/com/toyproject/blumarble/BluemarbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.blumarble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluemarbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
